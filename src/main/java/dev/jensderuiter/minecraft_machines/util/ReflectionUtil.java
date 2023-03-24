9d62ac48bc300aeba728d7f70a8c015693be9e26
Jens de Ruiter <jensgamesnl@gmail.com>
2023-03-24 19:50:39 +0100

    Simplify ReflectionUtil

--- src/main/java/dev/jensderuiter/minecraft_machines/util/ReflectionUtil.java
+++ src/main/java/dev/jensderuiter/minecraft_machines/util/ReflectionUtil.java
@@ -1,6 +1,7 @@
 package dev.jensderuiter.minecraft_machines.util;
 
 import dev.jensderuiter.minecraft_machines.MachinesPlugin;
+import dev.jensderuiter.minecraft_machines.machine.BaseMachine;
 import dev.jensderuiter.minecraft_machines.machine.Machine;
 import dev.jensderuiter.minecraft_machines.machine.RegisterMachine;
 import org.reflections.Reflections;
@@ -13,29 +14,21 @@ import java.util.Set;
 
 public class ReflectionUtil {
 
-    public static Set<Class> findAllMachines() {
-        String packageName = "dev.jensderuiter.minecraft_machines.machine";
-        Reflections reflections = new Reflections(packageName, new SubTypesScanner(false));
-        return new HashSet<>(reflections.getSubTypesOf(Object.class));
-    }
-
     public static void registerMachineTypes() {
-        Set<Class> machineClasses = findAllMachines();
-        for (Class clazz : machineClasses) {
-            RegisterMachine classAnnotation = (RegisterMachine) clazz.getAnnotation(RegisterMachine.class);
-            if (classAnnotation != null) {
-                try {
-                    Method nameMethod = clazz.getDeclaredMethod("getName");
-                    String name = (String) nameMethod.invoke(null);
-                    if (name == null) {
-                        throw new RuntimeException(clazz.getSimpleName() + " doesn't override a getName() function.");
-                    }
-                    MachinesPlugin.machineTypes.put(name, clazz);
-                } catch (NoSuchMethodException e) {
+        Set<Class<?>> machineClasses = new Reflections("dev.jensderuiter.minecraft_machines.machine")
+                .getTypesAnnotatedWith(RegisterMachine.class);
+        for (Class<?> clazz : machineClasses) {
+            try {
+                Method nameMethod = clazz.getDeclaredMethod("getName");
+                String name = (String) nameMethod.invoke(null);
+                if (name == null) {
                     throw new RuntimeException(clazz.getSimpleName() + " doesn't override a getName() function.");
-                } catch (InvocationTargetException | IllegalAccessException e) {
-                    throw new RuntimeException(e);
                 }
+                MachinesPlugin.machineTypes.put(name, (Class<? extends BaseMachine>) clazz);
+            } catch (NoSuchMethodException e) {
+                throw new RuntimeException(clazz.getSimpleName() + " doesn't override a getName() function.");
+            } catch (InvocationTargetException | IllegalAccessException e) {
+                throw new RuntimeException(e);
             }
         }
     }
